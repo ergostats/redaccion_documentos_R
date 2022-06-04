bc11684c916c761e851fa7eb8e1c9b1149943fa3
Alex Bajaña <alexb.inf.4891@gmail.com>
2022-06-04 13:09:44 -0500

    Añadí mi script de ejemplo 1

---
+++ 01_bienvenida_ayuda/mi_primer_script.R
@@ -0,0 +1,63 @@
+
+# Este es mi primer archivo de R ------------------------------------------
+
+
+# Mi nombre es: Alex Bajaña
+# La fecha de hoy es: 04 de junio de 2022
+
+help.start() 
+
+help.search("linear model")
+
+R.version
+
+# Este es mi primer vector. Para 4 individuos voy a coleccionar sus edades,
+# en años enteros
+
+edades <- c(54,23,17,90) # Aqui está mal definido el tipo de vector
+
+class(edades)
+
+edades <- c(54L,23L,17L,90L) # Aqui está bien :)
+
+class(edades)
+
+length(edades)
+
+nombres <- c("Alex","Wladimir","Diego","Libinton")
+
+  class(nombres)
+
+attributes(nombres)
+
+
+# Un vector de cada tipo: character, numeric, integer, logical
+# Luego por pares de tipos van a unir con c()
+
+
+
+# character, numeric
+
+# character,  integer
+
+vector_1 <- c(edades,nombres)
+
+class(vector_1)
+
+# character,  logical
+
+# Vector logico con una condicion:
+
+vector_logico <- edades > 23
+
+# logical, numeric
+
+vector_4 <- c(vector_logico,c(7L,9L))
+
+class(vector_4)
+# logical, integer
+
+
+vector_5 <- c(vector_logico,c(7.7,9.1))
+
+class(vector_5)
