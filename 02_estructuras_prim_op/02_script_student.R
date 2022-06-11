8db14192db6b542cac36a64982c89ddd57f186d5
Alex Bajaña <alexb.inf.4891@gmail.com>
2022-06-11 10:59:36 -0500

    Cambios iniciales

---
+++ 02_estructuras_prim_op/02_script_student.R
@@ -0,0 +1,97 @@
+#===============================================================================
+# Redacción de artículos académicos con R
+# 02. Estructuras y primeras operaciones
+# Estudiante
+#===============================================================================
+
+# Clase 2:Estructuras y primeras operaciones ------------------------------
+
+
+# Manipulacion de un vector -----------------------------------------------
+
+# Ejercicio 1:
+# 1. Crear un vector (cualquiera) de 5 elementos  
+# 2. Extraer elemento de la posición 1
+# 3. Extraer elementos de la posición 3 y 5
+
+
+# R como calculadora ------------------------------------------------------
+
+# Ejercicio 2:
+# 1.Crear 2 vectores numericos con 6 elementos
+# 2. Sumar los dos vectores
+# 3. Restar los dos vectores
+# 4. Multiplicar los dos vectores
+# 5. Dividir los dos vectores
+# Nota: Escribir el código de tal forma que se muestren los resultados
+
+
+# Operaciones estadísticas básicas ----------------------------------------
+
+# Ejercicio 3:
+# 1. Sumas los elementos de cada vector (usar los vectores creados en el ejercicio 2)
+# 2. Obtener el valor máximo de cada vector
+# 2. Obtener el percentil 45 de cada vector
+
+
+# Operadores lógicos ------------------------------------------------------
+
+# Ejercicio 4:
+# 1. Filtrar el primer vector usando el operador lógico: >=
+# 2. Filtrar el segundo vector usando dos operadores lógicos: >= <=
+
+
+# Fechas ------------------------------------------------------------------
+
+# Ejercicio 5:
+# 1. Crear un vector con fechas
+# 2. Obtener la clase del vector creado
+# 3. Convertir el vector a fecha
+# 4. Obtener los atributos del vector final
+
+
+
+## Horas------------------------------------------------------------------------
+
+# Ejercicio 6:
+# Obtener la hora actual con el uso horario de europa, Paris
+# Obtener la hora actual con el uso horario de America, New_York
+
+
+# Crear muestras aleatorias -----------------------------------------------
+
+# Ejercicio 7
+# 1. Crear un vector aleatorio de tipo caracter, de tamaño 100 y graficar
+# 2. Transformar a factor
+# 3. Crear un vector aleatorio de tipo numérico, de tamaño 100 
+# 4. Transformar a factor
+
+
+# Listas ------------------------------------------------------------------
+
+# Ejercicio 8
+# 1. Crear una lista con un elemento: caracter, entero, lógico y numérico
+# 2. Mostrar los elementos de esa lista
+# 3- Extraer los elementos de la lista
+# 4. Extraer un elemento de la lista pero, como lista
+
+
+# Tablas o data.frames ----------------------------------------------------
+
+# Ejercicio 9: usar el data frame mtcars 
+# 1. Ver la clase del data frame
+# 2. Ver los atributos del data frame
+# 3. Ver la dimensión
+# 4. Extraer de la tercera fila de la tabla:
+# 5. Extraer la fila 1,2 y las columnas 2 y 3 
+# 6. Crear una nueva variable en la tabla que sea la suma de la variable: carb
+
+
+# Filtrar una tabla -------------------------------------------------------
+
+# Ejercicio 9: usar el data frame iris
+# 1. Filtrar la base 1 variable: en la variable "Species" se filtre solo por "setosa"
+# 2. Filtrar la base 2 variables: Sepal.Length mayor a 4.9 y Sepal.Width menor a 3.6
+
+
+
