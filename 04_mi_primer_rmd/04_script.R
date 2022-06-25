427bfb8085d3aa210058318e0def77d749baeb81
Alex Bajaña <alexb.inf.4891@gmail.com>
2022-06-25 09:21:02 -0500

    Material para la clase 7 del 25 de junio de 2022

---
+++ 04_mi_primer_rmd/04_script.R
@@ -0,0 +1,81 @@
+#===============================================================================#
+# Redacción de artículos académicos con R
+# 04. Caso de Estudio de Género
+#===============================================================================#
+
+
+#### Formando la base de datos: ENEMDU dic, 2019 --------------------------
+
+# Metadato de la base -----------------------------------------------------
+
+# Como minimo debemos tener:
+
+identificacion <- list(pais="Ecuador",
+                       titulo="Encuesta Nacional de Empleo, Desempleo y Subempleo - Diciembre 2019",
+                       subtitulo="RONDA LXVI-12-2019",
+                       tipo_estudio="Encuesta Fuerza Laboral",
+                       tipo_dato="Encuesta por muestreo (ssd)",
+                       unidad_de_analisis=c("Vivienda","Hogar","Personas miembros del hogar"),
+                       alcance_tecnico=c("Información de los Miembros del Hogar",
+                                         "Características ocupacionales","Ingresos",
+                                         "Datos de la Vivienda y el Hogar"),
+                       cobertura=c("23 provincias del Ecuador",
+                                   "5 ciudades: Quito, Guayaquil, Cuenca, Machala, Ambato",
+                                   "exceptúa a Galápagos"))
+identificacion
+str(identificacion)
+
+
+# Crear Directorios: ------------------------------------------------------
+
+    # Crear carpeta de datos:
+
+  
+  "04_caso_estudio/datos/"
+  
+
+    # Crear la carpeta: Diccionarios:
+  
+  "04_caso_estudio/diccionarios/"
+
+# Descargar la base de datos ----------------------------------------------
+  # Enlace: 
+  "https://www.ecuadorencifras.gob.ec/documentos/web-inec/EMPLEO/2019/Diciembre/BDD_ENEMDU_2019_12_CSV%20%28DATOS%20ABIERTOS%29.zip"
+
+  # Archvivo:
+  enemdu_file <- "04_caso_estudio/enemdu_2019_12.zip"
+                
+
+
+# Revisar contenido
+
+  unzip()
+
+
+# Base de personas --------------------------------------------------------
+
+# Descomprimir y Extraer: base personas
+unzip()
+
+# Crear directorio: datos
+file.copy()
+
+
+# Diccionarios ------------------------------------------------------------
+
+# Descomprimir y Extraer: diccionarios
+unzip()
+
+# Crear directorio: diccionarios
+unzip()
+
+
+
+# Eliminar archivos -------------------------------------------------------
+file.remove()
+
+
+
+
+
+
